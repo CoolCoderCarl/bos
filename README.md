ed6feb8bba8ab581089ed65dba4c2d1db97be60a
CoolCoderCarl <123qwekir2wl@gmail.com>
2022-05-29 22:02:33 +0300

    Add workflow, README.md, requirements.txt, main.py

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # bos
 Battle of Sexes
+
+This model is pretty simple, the spouses try to decide where to go.
+
+They have two choices or strategies, and they prefer diffirent but no one want to be alone.
+
+Strategies are selected simultaneously.
+
+## Prehistory
+Always wonder about Game Theory.
+
+> Decided to continue write programs in Game Theory domain.  
+> (c) Author
+
+Hard choice for the lovers and spouses.
+
+Enjoy.
---
+++ main.py
@@ -0,0 +1,35 @@
+import random
+from typing import List
+
+spouses = {"wife": "musical", "husband": "football"}
+
+
+def wife_choice() -> str:
+    return random.choice(list(spouses.values()))
+
+
+def husband_choice() -> str:
+    return random.choice(list(spouses.values()))
+
+
+def compare_choices() -> List[int]:
+    w_c = wife_choice()
+    h_c = husband_choice()
+    if w_c == h_c:
+        print("Both win !")
+        if w_c == spouses["wife"]:
+            print("Wife choose " + w_c)
+            print("Husband choose " + h_c)
+            return [2, 1]
+        else:
+            print("Wife choose " + w_c)
+            print("Husband choose " + h_c)
+            return [1, 2]
+    else:
+        print("Wife choose " + w_c)
+        print("Husband choose " + h_c)
+        return [0, 0]
+
+
+if __name__ == "__main__":
+    print(compare_choices())
